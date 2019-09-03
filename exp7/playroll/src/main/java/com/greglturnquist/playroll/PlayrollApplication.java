3b8a2069a80ed9a1a918263c1c643ebe94309d92
dinithchanuka <dinithchanuka22@gmail.com>
2019-09-03 17:10:36 +0530

    Parrot final

---
+++ exp7/playroll/src/main/java/com/greglturnquist/playroll/PlayrollApplication.java
@@ -0,0 +1,13 @@
+package com.greglturnquist.playroll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlayrollApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlayrollApplication.class, args);
+	}
+
+}
